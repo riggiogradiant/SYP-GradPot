383d2dd79fa6a4e385d2f282133773521c6ff2c0
riggiogradiant <criggio@gradiant.org>
2023-07-14 14:51:39 +0200

    intento docker

--- SSH/server2.py
+++ SSH/server2.py
@@ -1,113 +1,136 @@
-import os
+# import socket
+
+# # Configuración del servidor
+# host = '127.0.0.1'  # Dirección IP del servidor
+# port = 8080  # Puerto para la conexión
+
+# # Crear un socket TCP/IP
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# # Vincular el socket al host y puerto especificados
+# sock.bind((host, port))
+
+# # Escuchar las conexiones entrantes (máximo de 1 conexión en espera)
+# sock.listen(1)
+
+# print('Servidor TCP escuchando en {}:{}'.format(host, port))
+
+# while True:
+#     # Esperar una conexión entrante
+#     print('Esperando conexiones...')
+#     client_sock, client_addr = sock.accept()
+#     print('Conexión entrante desde {}:{}'.format(client_addr[0], client_addr[1]))
+
+#     # Recibir datos del cliente
+#     data = client_sock.recv(1024).decode('utf-8')
+#     print('Datos recibidos:', data)
+
+#     # Procesar los datos recibidos (opcional)
+#     response = '¡Hola cliente! He recibido tus datos: {}'.format(data)
+
+#     # Enviar la respuesta al cliente
+#     client_sock.sendall(response.encode('utf-8'))
+#     print('Respuesta enviada:', response)
+
+#     # Cerrar la conexión con el cliente
+#     client_sock.close()
+
 import paramiko
 import socket
-import sys
 import threading
 import subprocess
-import random
-import logging
+import os
 
-# Configurar el registro
-log_file = './Server_Files/server_log.txt'
-logging.basicConfig(filename=log_file, level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+# Clase de servidor SSH
+def run_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+        return output.decode('utf-8').strip()
+    except subprocess.CalledProcessError as e:
+        return 'Error al ejecutar el comando: {}'.format(e.output.decode('utf-8').strip())
 
-CWD = os.path.dirname(os.path.realpath(__file__))
-HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'id_rsa'))
-
-# Leer usuarios y contraseñas válidos
-def parse_file(file_path):
-    usernames = []
-    passwords = []
-
-    with open(file_path, 'r') as file:
-        for line in file:
-            parts = line.strip().split(':')
-            if len(parts) >= 2:
-                usernames.append(parts[0])
-                passwords.append(parts[2])
-
-    return usernames, passwords
-
-
-# Clase servidor con funciones para verificar el canal y el usuario/contraseña
-class Server(paramiko.ServerInterface):
+class SSHServer(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
+
+    def check_auth_password(self, username, password):
+        # Verificar las credenciales del cliente
+        if username == 'user' and password == 'pass':
+            return paramiko.AUTH_SUCCESSFUL
+        return paramiko.AUTH_FAILED
 
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
-    def check_auth_password(self, username, password):
-        usernames, passwords = parse_file('./Server_Files/INFO_SERVER/etc/userdb.txt')
+    def check_channel_shell_request(self, channel):
+        self.event.set()
+        return True
 
-        if username in usernames and password in passwords:
-            if usernames.index(username) == passwords.index(password):
-                # Registro de información (info log)
-                # Generamos un número aleatorio de 6 dígitos para identificar cada conexión
-                connection_ID = random.randint(100000, 999999)
-                logging.info("Successful Login - User: %s - Password: %s - ID: %s", username, password, connection_ID)
-                return paramiko.AUTH_SUCCESSFUL
+# Configuración del servidor SSH
+host = '127.0.0.1'  # Dirección IP del servidor
+port = 8080  # Puerto SSH
+CWD = os.path.dirname(os.path.realpath(__file__))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'id_rsa'))
 
-        # Registro de información (info log)
-        logging.info("Failed Login - User: %s - Password: %s", username, password)
-        return paramiko.AUTH_FAILED
+# Crear un socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Vincular el socket al host y puerto especificados
+sock.bind((host, port))
 
-if __name__ == '__main__':
-    # IP y puerto donde se va a ejecutar el servidor
-    server = '127.0.0.1'
-    ssh_port = 22222
+# Escuchar las conexiones entrantes (máximo de 1 conexión en espera)
+sock.listen(1)
 
-    try:
-        # Creamos un socket TCP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((server, ssh_port))
-        sock.listen(100)
-        print('[+] Listening for connection...')
-        logging.info('[+] Listening for connection...')
-        while True:
-            client, addr = sock.accept()
-            print(f'[+] Got a connection from {addr}')
-            logging.info(f'[+] Got a connection from {addr}')
-            bhSession = paramiko.Transport(client)
-            bhSession.add_server_key(HOSTKEY)
-            server = Server()
-            bhSession.start_server(server=server)
+print('Servidor SSH escuchando en {}:{}'.format(host, port))
 
-            # Esperamos a que el cliente abra un canal SSH
-            chan = bhSession.accept(20)
+# Esperar una conexión entrante
+print('Esperando conexiones...')
+client_sock, client_addr = sock.accept()
+print('Conexión entrante desde {}:{}'.format(client_addr[0], client_addr[1]))
 
-            if chan is None:
-                print('*** No Channel.')
-                logging.info('*** No Channel.')
-                sys.exit(1)
+try:
+    # Crear el servidor SSH
+    ssh_server = SSHServer()
 
-            print('[+] Authenticated!')
-            print(chan.recv(1024).decode())
-            logging.info('[+] Authenticated!')
+    # Configurar el transporte SSH
+    transport = paramiko.Transport(client_sock)
+    print('ANTES DEL HOSTKEY')
+    transport.add_server_key(HOSTKEY)  # Agregar una clave SSH (opcional)
+    print('DESPUES DEL HOSTKEY')
+    transport.start_server(server=ssh_server)
+    print('DESPUES DE INICIAR EL SERVER')
 
-            print('============================================')
+    # Esperar a que se establezca una sesión
+    channel = transport.accept(10)
+    print('SE CREÓ LA CONEXIÓN')
+    print(channel)
 
-            try:
-                while True:
-                    print('Waiting for message from Client ...')
-                    cmd_from_client = chan.recv(1024).decode()
-                    print('Mensaje recibido:', cmd_from_client, '\n')
+    if channel is not None:
+        print("Estoy dentro del channel not None")
+        s_serv_eve = ssh_server.event.wait(10)
+        print(s_serv_eve)  # Esperar hasta 10 segundos para la shell interactiva
+        print("Se creó la shell interactiva")
 
-                    if cmd_from_client != '':
-                        # Enviamos el comando al main y él se encargará de devolver la salida al cliente
-                        output = subprocess.check_output(["python3", "../main.py", cmd_from_client])
-                        output = output.decode().strip()
-                        output = output.encode()
-                        chan.send(output)
+        # Si se ha establecido la shell interactiva, redirigir la entrada/salida/err
+        #if ssh_server.event.is_set():
+        if s_serv_eve == 'True':
+            channel.send('¡Bienvenido a la shell SSH!\r\n')
+            channel.send('Ingrese comandos y presione Enter para ejecutarlos.\r\n')
 
-            except KeyboardInterrupt:
-                bhSession.close()
+            while True:
+                command = channel.recv(1024).decode('utf-8').strip()
+                if command == 'exit':
+                    break
+                # Ejecutar el comando y enviar la salida al cliente
+                output = run_command(command)
+                channel.send(output + '\r\n')
 
-    except Exception as e:
-        print('[-] Listen failed' + str(e))
-        sys.exit(1)
+            channel.close()
+
+except Exception as e:
+    print('Error:', str(e))
+
+finally:
+    client_sock.close()
